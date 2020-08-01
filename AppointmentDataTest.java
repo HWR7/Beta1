7220da1990dd3583cbaa041d8b5f300d24378dab
Duysburg <66062396+Duysburg@users.noreply.github.com>
2020-08-01 16:52:48 +0200

    Add files via upload

--- AppointmentDataTest.java
+++ AppointmentDataTest.java
@@ -6,11 +6,16 @@ public class AppointmentDataTest {
 
     public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 
+        PersonData pdb = new PersonData(10, 100);
+        pdb.insertStylist("Chanti", "Mueller", "c.mueller@mail.de", 123123, "cut", "wash");
+        pdb.insertClient("Chanto", "Mueller", "c.mueller@mail.de", 123123);
+        pdb.insertStylist("Chanti", "Mueller", "c.mueller@mail.de", 123123, "cut", "wash");
+        pdb.insertClient("Chanto", "Mueller", "c.mueller@mail.de", 123123);
+        pdb.saveAccounts("Konten.dat");
 
         AppointmentData adb = new AppointmentData(100);
         //Appointment a1 = new Appointment(1, 2, "wash");
-
-        adb.insertAppointment(1,2,3,15, 30, 1, 1, "wash");
+        //adb.insertAppointment(1,2,3,15, 30, 1, 1, "wash");
         //adb.insertAppointment(2,3,4,15, 30, 1, 1,"wash");
         adb.insertAppointment(1,2,3,15, 30, 1, 1,"wash");
         adb.insertToday(13,15, 1, 1,"wash");
@@ -30,8 +35,8 @@ public class AppointmentDataTest {
 
         adb.saveAppointments("Termine.dat");
         System.out.println("save: ");
-        adb.organizeAndDeleteExpiredData();
-        //adb.bubbleSort("Termine.dat");
+        //adb.organizeAndDeleteExpiredData();
+        adb.bubbleSort("Termine.dat");
         adb.display();
 
         /*
